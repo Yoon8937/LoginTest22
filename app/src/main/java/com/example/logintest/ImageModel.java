91a36a9b670eee805a8268f142897b573d128d6d
Yoon8937 <cyg8937@gmail.com>
2022-02-03 12:47:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/logintest/ImageModel.java
@@ -0,0 +1,9 @@
+package com.example.logintest;
+
+public class ImageModel {
+    private String model_pic;
+
+    public ImageModel(String model_pic){
+        this.model_pic = model_pic;
+    }
+}
---
+++ app/src/main/java/com/example/logintest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.logintest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button signup = (Button) findViewById(R.id.signupbtn);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Camera_main.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
